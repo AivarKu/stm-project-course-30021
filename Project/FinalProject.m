bd8d570ef053123fdde7103bb810dcef372c8ed0
AivKul <aivar.klle@protonmail.com>
2021-11-30 13:46:48 +0100

    Update to Matlab script

--- Project/FinalProject.m
+++ Project/FinalProject.m
@@ -5,11 +5,11 @@ format compact
 %% Created on 27.10.2021
 COM_PORT = 'COM4';
 
-oldSerial = instrfind('Port', COM_PORT); 
-if (~isempty(oldSerial))  % if the set of such objects is not(~) empty 
-    disp('WARNING:  COM in use.  Closing.') 
-    delete(oldSerial) 
-end 
+oldSerial = instrfind('Port', COM_PORT);
+if (~isempty(oldSerial))  % if the set of such objects is not(~) empty
+    disp('WARNING:  COM in use.  Closing.')
+    delete(oldSerial)
+end
 
 ser = serial(COM_PORT, 'baudrate', 9600, 'terminator', 'CR');
 fopen(ser);
@@ -17,15 +17,93 @@ fopen(ser);
 if 0
     % 2D arc scanner
     figure
-    subplot(2,2,1)
+% %     subplot(2,2,1)
+    scatter3(0,0,0,'b*')
+    axis([-0.5 0.5 -0.5 0.5 -0.5 0.5])
+    xlabel('x')
+    ylabel('y')
+    zlabel('z')
     hold on
-    
-    %addpoints
-    
-    while(1)
-        
+
+    %Angles
+    N1 = 40;
+    N2 = 40;
+
+    N1x = normpdf(N1,50,sqrt(100));
+    N2x = normpdf(N2,35,sqrt(100));
+
+    N1deg = linspace(3*pi/4,pi/4,N1);
+    N2deg = linspace(3*pi/4,pi/4,N2);
+
+%     figure
+%         plot(N1,N1x);
+%         grid on
+%
+    n1 = 1;
+    n2 = 1;
+
+    goingRight = 1;
+
+    points = zeros(N1*N2,3);
+
+%     while(1)
+    for k = 1:1000
+        n1 = n1 + 1;
+        if n1 == N1+1
+           n1 = 1;
+           n2 = n2 + 1;
+
+           if (n2 == N2+1)
+               break
+           end
+        end
+
+        r = 0.5;
+        th1 = N1deg(n1);
+        th2 = N2deg(n2);
+        x = r*sin(th1)*cos(th2);
+        y = r*sin(th1)*sin(th2);
+        z = r*cos(th1);
+%         plot3(x,y,z,'bo')
+        points(k,1) = x;
+        points(k,2) = y;
+        points(k,3) = z;
+        scatter3(x,y,z,'bo')
+
+        drawnow()
     end
-    
+
+%     figure
+%     h = animatedline;
+%     axis([0 4*pi -1 1])
+%
+%     for x = 1:1000
+%         n1 = n1 + 1;
+%         if n1 == N1+1
+%            n1 = 1;
+%            n2 = n2 + 1;
+%
+%            if (n2 == N2+1)
+%                break
+%            end
+%         end
+%
+%         r = 0.5;
+%         th1 = N1deg(n1);
+%         th2 = N2deg(n2);
+%         x = r*cos(th1)*sin(th2);
+%         y = r*sin(th1)*sin(th2);
+%         z = r*cos(th2);
+%         addpoints(h,x,y,z)
+%         grid on
+%         xlabel('x')
+%         ylabel('y')
+%         zlabel('z')
+%
+%
+%         drawnow limitrate
+%     end
+
 else
     disp('1D lidar stared')
     % 1D radar
@@ -46,7 +124,7 @@ else
 
         a = fscanf(ser, '%f');
         if abs(a) < 1e-4
-           continue; 
+           continue;
         end
 
         rho(thetaCnt) = a;
@@ -64,4 +142,4 @@ else
 end
 
 delete(ser)
-clear ser 
+clear ser
